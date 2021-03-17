6f4f148e754715f805ecbf140c3fccca4646b308
kalwalt <github@kalwaltart.it>
2021-03-17 16:50:48 +0100

    image with descriptors is displayed now

--- www/index.js
+++ www/index.js
@@ -1,6 +1,5 @@
 import * as wasm from "wasm-akaze-example";
 
-console.log(wasm);
 
 wasm.print_test(2);
 
@@ -10,15 +9,22 @@ function getImage(url){
             var img = new Image()
             const canvas = document.getElementById('cv')
             const ctx = canvas.getContext('2d')
+            const canvasAkaze = document.getElementById('cv-akaze')
+            const ctxAkz = canvasAkaze.getContext('2d')
             img.onload = function(){
                 resolve(url)
                 canvas.width = img.width
                 canvas.height = img.height
                 ctx.drawImage(img, 0, 0)
                 let imgData = ctx.getImageData(0,0,canvas.width, canvas.height)
-                console.log(imgData);
                 let raw = wasm.open_from_buffer(imgData.data,canvas.width,canvas.height)
-                console.log(raw)
+
+                canvasAkaze.width = canvas.width
+                canvasAkaze.height = canvas.height
+
+                let imageData = new ImageData(canvasAkaze.width, canvasAkaze.height)
+                imageData.data.set(raw);
+                ctxAkz.putImageData(imageData, 0, 0);
             }
             img.onerror = function(){
                 reject(url)
